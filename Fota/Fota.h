b9679a8e3dc707fd1d4edd97320ffca9017d50d2
SeanXiaoby <sean_xiaoby@163.com>
2022-11-08 21:01:37 -0800

    Update

---
+++ Fota/Fota.h
@@ -0,0 +1,66 @@
+/**********************************************************************
+* COPYRIGHT 2018 MULTI-TECH SYSTEMS, INC.
+*
+* ALL RIGHTS RESERVED BY AND FOR THE EXCLUSIVE BENEFIT OF
+* MULTI-TECH SYSTEMS, INC.
+*
+* MULTI-TECH SYSTEMS, INC. - CONFIDENTIAL AND PROPRIETARY
+* INFORMATION AND/OR TRADE SECRET.
+*
+* NOTICE: ALL CODE, PROGRAM, INFORMATION, SCRIPT, INSTRUCTION,
+* DATA, AND COMMENT HEREIN IS AND SHALL REMAIN THE CONFIDENTIAL
+* INFORMATION AND PROPERTY OF MULTI-TECH SYSTEMS, INC.
+* USE AND DISCLOSURE THEREOF, EXCEPT AS STRICTLY AUTHORIZED IN A
+* WRITTEN AGREEMENT SIGNED BY MULTI-TECH SYSTEMS, INC. IS PROHIBITED.
+*
+***********************************************************************/
+
+#ifndef FOTA_H
+#define FOTA_H
+#include "mDot.h"
+#ifdef FOTA
+#include "FragmentationSession.h"
+#endif
+#include "MulticastGroup.h"
+#include "ApplicationLayerPackage.h"
+
+class Fota {
+
+    public:
+        Fota(mDot* dot);
+        ~Fota();
+
+        static Fota* getInstance(mDot* dot);
+        static Fota* getInstance();
+        void processCmd(uint8_t* payload, uint8_t port, uint8_t size);
+        void reset();
+        bool idle();
+        bool txPending();
+        void enable(bool enabled);
+        bool enable();
+        void fixEventQueue();
+        bool ready();
+        int32_t timeToStart();
+        bool getClockSynced();
+        int32_t setClockOffset(uint32_t gps_time);
+
+    private:
+        static void start();
+
+        bool _enabled;
+        Thread _send_thread;
+        uint8_t p[242];
+        static Fota* _instance;
+        mDot* _dot;
+        bool _clk_synced = false;
+#ifdef FOTA
+        FragmentationSession* _frag_session;
+#endif
+        MulticastGroup* _mc_group;
+
+        struct {
+            ApplicationMessage rx;
+            ApplicationMessage tx;
+        } _msg;
+};
+#endif
---
+++ libxDot/Fota/MulticastGroup/MulticastGroup.h
@@ -0,0 +1,98 @@
+/**********************************************************************
+* COPYRIGHT 2018 MULTI-TECH SYSTEMS, INC.
+*
+* ALL RIGHTS RESERVED BY AND FOR THE EXCLUSIVE BENEFIT OF
+* MULTI-TECH SYSTEMS, INC.
+*
+* MULTI-TECH SYSTEMS, INC. - CONFIDENTIAL AND PROPRIETARY
+* INFORMATION AND/OR TRADE SECRET.
+*
+* NOTICE: ALL CODE, PROGRAM, INFORMATION, SCRIPT, INSTRUCTION,
+* DATA, AND COMMENT HEREIN IS AND SHALL REMAIN THE CONFIDENTIAL
+* INFORMATION AND PROPERTY OF MULTI-TECH SYSTEMS, INC.
+* USE AND DISCLOSURE THEREOF, EXCEPT AS STRICTLY AUTHORIZED IN A
+* WRITTEN AGREEMENT SIGNED BY MULTI-TECH SYSTEMS, INC. IS PROHIBITED.
+*
+***********************************************************************/
+
+#ifndef MULTICASTGROUP_H
+#define MULTICASTGROUP_H
+#include "mDot.h"
+#include "mbed.h"
+#include "ApplicationLayerPackage.h"
+#define GPS_EPOCH 315964800U
+#define MULTICAST_SESSIONS 3
+
+
+class MulticastGroup : public ApplicationLayerPackage {
+    public:
+        MulticastGroup(mDot* dot);
+        ~MulticastGroup();
+        void reset();
+        //void newTime();
+        uint8_t getPort();
+        void processCmd(ApplicationMessage& recv, ApplicationMessage& resp);
+        int32_t timeToStart();
+        void fixEventQueue();
+        void setClockOffset(int32_t offset);
+        bool switchClassIfPending();
+        void switchClass();
+        bool isClassSwitchActive() const;
+    private:
+        enum MulticastCommands {
+            PACKAGE_VERSION,
+            STATUS,
+            SETUP,
+            DELETE,
+            CLASS_C_SESSION,
+            CLASS_B_SESSION_REQ
+        };
+
+        typedef struct {
+            bool valid;
+            uint8_t dr;
+            uint8_t fragGroup;
+            uint16_t timeout;
+            uint32_t tme;
+            uint32_t freq;
+            uint32_t addr;
+            uint32_t max_frame_count;
+            us_timestamp_t timetostart;
+            int32_t class_c_end;
+            int32_t class_c_start;
+            char dev_class;
+            time_t time_setup;
+            int8_t periodicity;
+        } mcgroup;
+
+        bool* _filled;
+        uint8_t _groupId;
+        uint8_t _ans;
+        uint8_t _delay;
+        uint8_t _token;
+        uint8_t _dr;
+        uint32_t _freq;
+        uint32_t _frame_count;
+        time_t _now;
+        int32_t _clk_sync;
+
+        // Thread _event_thread;
+        // EventQueue _switch_class_queue;
+        mcgroup _mcGroup[MULTICAST_SESSIONS];
+        char _org_class;
+        Mutex _class_switch_lock;
+
+        struct {
+            mcgroup* group;
+            bool active;
+            bool pending;
+            LowPowerTimeout timer;
+        } _class_switch;
+
+        void setupClassB(uint8_t id);
+        void setupClassC(uint8_t id);
+        void setupClassSwitch(uint8_t id);
+
+        void pendClassSwitch();
+};
+#endif
